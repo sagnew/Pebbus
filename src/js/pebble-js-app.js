4d0ba03d18bf98bae4f2de19317436a32a607be4
Sam Agnew <sagnew92@gmail.com>
2014-01-14 20:40:05 -0800

    Displays nearby stops
    WIth hardcoded coordinates for now

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,5 +1,25 @@
+var getNearbyStops = function(){
+	var req = new XMLHttpRequest();
+  	req.open('GET', 'http://runextbus.herokuapp.com/nearby/40.5040/-74.44905', true);
+	req.onload = function(e) {
+		if (req.readyState == 4 && req.status == 200) {
+			if(req.status == 200) {
+				var response = JSON.parse(req.responseText);
+				var stops = [];
+				for(var k in response) {
+					 if(response.hasOwnProperty(k)){
+						  stops.push(k);
+					 }
+				}
+				Pebble.showSimpleNotificationOnPebble("Nearby stops", stops.join(' '));
+			} else { console.log("Error"); }
+		}
+	}
+	req.send(null);
+}
+
 Pebble.addEventListener("ready",
-  function(e) {
-    Pebble.showSimpleNotificationOnPebble("Hello", "World");
-  }
+    function(e) {
+	    getNearbyStops();
+    }
 );
