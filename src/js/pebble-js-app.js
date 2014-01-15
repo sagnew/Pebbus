a32b3cd582a1ba0bd1239bed5ccd3098c5737dfe
Sam Agnew <sagnew92@gmail.com>
2014-01-14 20:55:13 -0800

    Broke up tasks into several functions

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,25 +1,33 @@
-var getNearbyStops = function(){
+var executeGetRequest = function(url, callback){
 	var req = new XMLHttpRequest();
-  	req.open('GET', 'http://runextbus.herokuapp.com/nearby/40.5040/-74.44905', true);
+  	req.open('GET', url, true);
 	req.onload = function(e) {
 		if (req.readyState == 4 && req.status == 200) {
 			if(req.status == 200) {
 				var response = JSON.parse(req.responseText);
-				var stops = [];
-				for(var k in response) {
-					 if(response.hasOwnProperty(k)){
-						  stops.push(k);
-					 }
-				}
-				Pebble.showSimpleNotificationOnPebble("Nearby stops", stops.join(' '));
+				callback(response);
 			} else { console.log("Error"); }
 		}
 	}
 	req.send(null);
 }
 
+var getNearbyStops = function(response){
+	var stops = [];
+	for(var k in response) {
+		if(response.hasOwnProperty(k)){
+			stops.push(k);
+		}
+	}
+	Pebble.showSimpleNotificationOnPebble("Nearby stops", stops.join(' '));
+}
+
+var displayBusInfo = function(){
+	executeGetRequest('http://runextbus.herokuapp.com/nearby/40.5040/-74.44905', getNearbyStops);
+}
+
 Pebble.addEventListener("ready",
     function(e) {
-	    getNearbyStops();
+	    displayBusInfo();
     }
 );
